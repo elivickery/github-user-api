66cf5840e7ae4aefff97a1504370383e8df24d08
EJ Vickery <e.n.vickery@gmail.com>
2017-10-01 20:01:22 -0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require 'json'
 require 'rack/test/body/json'
 
 ENV['RACK_ENV'] = 'test'
-ENV['ACCESS_TOKEN'] = '7c2432de2a737b5e40558332ed7e6507b3ca735b'
 
 require File.expand_path '../../app/controllers/routes.rb', __FILE__
 
